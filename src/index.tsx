dc6dd3359d73d700990687f517509a6713fd69d0
min199973 <min199973@gmail.com>
2023-04-07 00:40:07 +0900

    ReactMaster #5.4 ~ #5.10

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <QueryClientProvider client={queryClient} contextSharing={true}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
+);
