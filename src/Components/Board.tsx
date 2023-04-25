8cd8121c67b439ac1421c391d80c135a1b7ebc33
min199973 <min199973@gmail.com>
2023-04-25 22:34:32 +0900

    ReactMaster #7.10 ~ #7.14

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,14 +1,18 @@
 import { Droppable } from "react-beautiful-dnd";
 import DragabbledCard from "./DragabbledCard";
 import styled from "styled-components";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { ITodo } from "../atoms";
 
 const Wrapper = styled.div`
   width: 300px;
-  padding: 20px 10px;
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h1`
@@ -19,22 +23,70 @@ const Title = styled.h1`
 `;
 
 interface IBoardProp {
-  toDos: string[];
+  toDos: ITodo[];
   boardId: string;
 }
 
+interface IAreaProps {
+  isDraggingOver: boolean;
+  draggingFromThisWith: boolean;
+}
+
+interface IForm {
+  toDo: string;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? "#dfe6e9"
+      : props.draggingFromThisWith
+      ? "#b2bece"
+      : "transparent"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+  padding: 20px;
+`;
+const Form = styled.form`
+  width: 100%;
+  input {
+    width: 100%;
+  }
+`;
+
 function Board({ toDos, boardId }: IBoardProp) {
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const onValid = ({ toDo }: IForm) => {
+    setValue("toDo", "");
+  };
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <Form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("toDo", { required: true })}
+          type="text"
+          placeholder={`Add task on ${boardId}`}
+        />
+      </Form>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, info) => (
+          <Area
+            isDraggingOver={info.isDraggingOver}
+            draggingFromThisWith={Boolean(info.draggingFromThisWith)}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
             {toDos.map((toDo, index) => (
-              <DragabbledCard key={toDo} index={index} toDo={toDo} />
+              <DragabbledCard
+                key={toDo.id}
+                index={index}
+                toDoId={toDo.id}
+                toDoText={toDo.text}
+              />
             ))}
             {magic.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
--- src/Components/DragabbledCard.tsx
+++ src/Components/DragabbledCard.tsx
@@ -3,28 +3,33 @@ import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
 interface IDragabbledCardProps {
-  toDo: string;
+  toDoId: number;
+  toDoText: string;
   index: number;
 }
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   background-color: ${(props) => props.theme.cardColor};
   border-radius: 5px;
   padding: 10px 10px;
   margin-bottom: 5px;
+  background-color: ${(props) =>
+    props.isDragging ? "#74b9ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 5px rgba(0,0,0,0.05)" : "none"};
 `;
 
-function DragabbledCard({ toDo, index }: IDragabbledCardProps) {
-  console.log(toDo, "has been rendered");
+function DragabbledCard({ toDoId, toDoText, index }: IDragabbledCardProps) {
   return (
-    <Draggable key={toDo} draggableId={toDo} index={index}>
-      {(magic) => (
+    <Draggable draggableId={toDoId + ""} index={index}>
+      {(magic, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={magic.innerRef}
           {...magic.draggableProps}
           {...magic.dragHandleProps}
         >
-          {toDo}
+          {toDoText}
         </Card>
       )}
     </Draggable>
